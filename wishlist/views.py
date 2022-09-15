93d6b2447e5ef25a5f530dc1a8562e0277c5ae77
raditya.aditama <raditya.aditama@ui.ac.id>
2022-09-15 12:12:34 +0700

    Update views.py and urls.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
+
     context = {
         'list_barang': data_barang_wishlist,
         'nama': 'Raditya Aditama'
     }
     return render(request, "wishlist.html", context)
-    
 
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
+def show_json(request):
+    data = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+
+    # Jika JSON
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 # Create your views here.
